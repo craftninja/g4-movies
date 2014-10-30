7056f65a25eb1623711e3d10132ee45d46ee0f98
EmilyPlatzer <emilyplatzer@gmail.com>
2014-10-30 14:17:25 -0600

    User can add movies and see them listed on index

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,18 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    movie_params = params.require(:movie).permit(:title, :rating)
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to movies_path
+  end
+
+end
